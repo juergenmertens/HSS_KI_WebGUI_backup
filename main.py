f856422b1e33d5e18d3736afa6176e3e2022b973
jrmertens36 <36986210+jrmertens36@users.noreply.github.com>
2022-11-08 23:33:46 +0100

    Bugs korrigiert

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from __init__ import create_app
 
 main = Blueprint('main', __name__)
 
-fields = ['Field 1', 'Field 2', 'Field 3', 'Field 4']
+fields = [['anz', 'Anzahl Beschädigungen'], ['tiefe', 'Durchschnittl. Tiefe in mm']]
 
 @main.route('/')
 def index_get():
-    return render_template('index.html')
+    return render_template('index.html', inputfields = fields )
 
 @main.route('/', methods=['Post'])
 def index_post():
-    return render_template('index.html')
+    return render_template('index.html', inputfields = fields)
 
 if __name__ == '__main__':
     create_app().run(debug=True)
