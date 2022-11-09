2ebba6f500b403767e57ecb7d7d89423771764f9
jrmertens36 <jm@jacnet.de>
2022-11-09 11:07:19 +0100

    Ergänzungen müssen in neuere Version auf Benni gemerged werden.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, render_template
 from __init__ import create_app
+import joblib
 
 main = Blueprint('main', __name__)
 
+# TODO
+# Eingabefelder eintragen
 fields = ['Field 1', 'Field 2', 'Field 3', 'Field 4']
+# TODO
+# Name des Modells festlegen
+model_file='model.dt'
 
 @main.route('/')
 def index_get():
@@ -11,7 +17,23 @@ def index_get():
 
 @main.route('/', methods=['Post'])
 def index_post():
-    return render_template('index.html')
+
+    # TODO
+    # Model laden
+    model = joblib.load(model_file)
+
+    # TODO
+    # Daten aus Form Request laden, evtl. konvertieren
+
+    # TODO
+    # Model mit Daten aufrufen
+    model.predict()
+
+    # TODO
+    # Ergebnis für Ausgabe vorbereiten
+    # result = ...
+
+    return render_template('index.html', inputfields = fields, result = result)
 
 if __name__ == '__main__':
     create_app().run(debug=True)
